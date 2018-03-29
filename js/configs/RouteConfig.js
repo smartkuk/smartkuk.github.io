3ce01b4a7eb22d4d246a19218ed86901ff30d991
SmartKuk <pipe0502@Smarts-MacBook-Pro.local>
2018-03-29 16:34:15 +0900

    test route

--- js/configs/RouteConfig.js
+++ js/configs/RouteConfig.js
@@ -3,22 +3,9 @@
  */
 app.config(function($routeProvider, $locationProvider) {
 	
-	$routeProvider.when('/Book/:bookId', {
-		
-		templateUrl: 'book.html',
-		controller: 'BookController',
-		resolve: {
-			delay: function($q, $timeout) {
-				var delay = $q.defer();
-				$timeout(delay.resolve, 1000);
-				return delay.promise;
-			}
-		}
-	}).when('/Book/:bookId/ch/:chapterId', {
-		
-		templateUrl: 'chapter.html',
-		controller: 'ChapterController'
+	$routeProvider.when('/routeTest', {
+		templateUrl: 'views/routeTest.html'
 	});
-
+	
 	$locationProvider.html5Mode(true);
 });
